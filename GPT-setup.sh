90dd9c4e118fc1f39a439c30a07ee34eb559fac9
FuzyTVSadBoy1337 <121013716+FuzyTVSadBoy1337@users.noreply.github.com>
2025-08-15 16:59:27 +0700

    Update GPT-setup.sh

--- GPT-setup.sh
+++ GPT-setup.sh
@@ -12,4 +12,4 @@ yes | pkg i python-pip
 pip install requests rich prettytable pytz
 export CFLAGS="-Wno-error=implicit-function-declaration"
 pip install psutil
-curl -Ls "https://cdn.jsdelivr.net/gh/FuzyTVSadBoy1337/GPT-setup@main/GPT-Tool-v2.py" -o /sdcard/Download/GPT_Tool-v2.py
+curl -Ls "https://cdn.jsdelivr.net/gh/FuzyTVSadBoy1337/GPT-setup@main/GPT-Tool-v2.py" -o /sdcard/Download/GPT-Tool-v2.py
